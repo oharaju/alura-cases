03ba54ef5635ba146221f741d358f951869e57cd
Júlia Ohara <juliaohara.br@gmail.com>
2023-05-08 22:34:46 -0300

    feat: add link component from next.js

---
+++ src/pages/contact/index.js
@@ -0,0 +1,10 @@
+import Link from 'next/link';
+
+export default function contact() {
+  return (
+    <div>
+      <h1>Alura Cases - Página de Perguntas FAQ</h1>
+      <Link href="/">Ir para a Home</Link>
+    </div>
+  )
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,10 @@
+import Link from 'next/link';
 
 export default function Home() {
   return (
-    <>
-      <div>
-        <h1>Alura Cases- Home</h1>
-      </div>
-    </>
+    <div>
+      <h1>Alura Cases- Home</h1>
+      <Link href="/contact">Ir para FAQ</Link>
+    </div>
   )
 }
