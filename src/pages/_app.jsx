774cb8efbb772dce690a34f4fa453ca38c08dd23
Júlia Ohara <juliaohara.br@gmail.com>
2023-05-18 22:47:28 -0300

    feat: add propType

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,5 +1,11 @@
 import '../styles/reset.css';
+import PropTypes from 'prop-types';
 
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />;
 }
+
+App.propTypes = {
+  Component: PropTypes.element.isRequired,
+  pageProps: PropTypes.element.isRequired,
+};
