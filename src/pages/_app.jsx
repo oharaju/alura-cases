83c5d2e6384a33267137025ee2e7c0704b02442d
Júlia Ohara <juliaohara.br@gmail.com>
2023-05-18 22:28:55 -0300

    feat: add css reset

---
+++ src/pages/_app.jsx
@@ -0,0 +1,5 @@
+import '../styles/reset.css';
+
+export default function App({ Component, pageProps }) {
+  return <Component {...pageProps} />;
+}
--- src/pages/contact/index.jsx
+++ src/pages/contact/index.jsx
@@ -1,12 +1,10 @@
-import NextLink from '../../components/NextLink'
+import NextLink from '../../components/NextLink';
 
 export default function contact() {
   return (
     <div>
       <h1>Alura Cases - Página de Perguntas FAQ</h1>
-      <NextLink href="/">
-        Ir para a Home
-      </NextLink>
+      <NextLink href='/'>Ir para a Home</NextLink>
     </div>
-  )
+  );
 }
