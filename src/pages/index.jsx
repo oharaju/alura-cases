fb8e5067f308f3e6f9125ac90d4b616c0d8dd12f
Júlia Ohara <juliaohara.br@gmail.com>
2023-05-18 21:55:44 -0300

    feat: tweak ESlint rules

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -4,9 +4,7 @@ export default function Home() {
   return (
     <div>
       <h1>Alura Cases- Home</h1>
-      <NextLink href="/contact">
-        Ir para FAQ
-      </NextLink>
+      <NextLink href='/contact'>Ir para FAQ</NextLink>
     </div>
-  )
+  );
 }
