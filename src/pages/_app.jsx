c6863f5635034543adf759e47b6fd386fa1be6bb
Júlia Ohara <juliaohara.br@gmail.com>
2023-05-29 21:44:04 -0300

    fix: add rule to next.config.js

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,11 +1,5 @@
 import '../styles/reset.css';
-import PropTypes from 'prop-types';
 
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />;
 }
-
-App.propTypes = {
-  Component: PropTypes.element.isRequired,
-  pageProps: PropTypes.element.isRequired,
-};
