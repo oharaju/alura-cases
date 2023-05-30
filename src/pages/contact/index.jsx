2bc2099ef3bb52a6273a94243742457a291c325e
Júlia Ohara <juliaohara.br@gmail.com>
2023-05-29 21:47:26 -0300

    feat: add styled component to contacts component

--- src/pages/contact/index.jsx
+++ src/pages/contact/index.jsx
@@ -1,10 +1,12 @@
 import NextLink from '../../components/NextLink';
+import { Button } from './styles';
 
 export default function contact() {
   return (
     <div>
       <h1>Alura Cases - Página de Perguntas FAQ</h1>
       <NextLink href='/'>Ir para a Home</NextLink>
+      <Button>Botão Vermelho</Button>
     </div>
   );
 }
