6f33552f3bde9b24931dff445608663f2d96613c
Júlia Ohara <juliaohara.br@gmail.com>
2023-06-30 21:51:49 -0300

    feat: add getStaticProps to render api content

--- src/pages/contact/index.jsx
+++ src/pages/contact/index.jsx
@@ -1,21 +1,23 @@
-import { useEffect, useState } from 'react';
 import NextLink from '../../components/NextLink';
 import { Button } from './styles';
 
-export default function contact() {
-  const [faq, setFaq] = useState([]);
+export async function getStaticProps() {
+  const faqApiUrl =
+    'https://gist.githubusercontent.com/omariosouto/0ceab54bdd8182cbd1a4549d32945c1a/raw/578ad1e8e5296fa048e3e7ff6b317f7497b31ad9/alura-cases-faq.json';
 
-  useEffect(() => {
-    const faqApiUrl =
-      'https://gist.githubusercontent.com/omariosouto/0ceab54bdd8182cbd1a4549d32945c1a/raw/578ad1e8e5296fa048e3e7ff6b317f7497b31ad9/alura-cases-faq.json';
+  const faq = await fetch(faqApiUrl)
+    .then((responseApiUrl) => responseApiUrl.json())
+    .then((response) => response);
 
-    fetch(faqApiUrl)
-      .then((responseApiUrl) => responseApiUrl.json())
-      .then((response) => {
-        setFaq(response);
-      });
-  }, []);
+  return {
+    props: {
+      nome: 'Júlia',
+      faq,
+    },
+  };
+}
 
+export default function contact({ faq }) {
   return (
     <div>
       <h1>Alura Cases - Página de Perguntas FAQ</h1>
