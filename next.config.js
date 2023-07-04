53533def8102be535c47677bee8673c1e95fc065
Júlia Ohara <juliaohara.br@gmail.com>
2023-07-03 22:09:17 -0300

    feat: feat: add url redirect for contacts

--- next.config.js
+++ next.config.js
@@ -4,4 +4,13 @@ module.exports = {
   },
   reactStrictMode: true,
   trailingSlash: true,
+  async redirects() {
+    return [
+      {
+        source: '/faq',
+        destination: '/contact',
+        permanent: true,
+      },
+    ];
+  },
 };
